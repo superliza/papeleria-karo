85c9fd650b6a4c0152901e3ed06b91bb5ed27900
Gail Keegan Lopez Monares <gailkeeganlopez@hotmail.com>
2020-03-27 19:25:29 -0600

    logo del equipo

---
+++ logo.c
@@ -0,0 +1,40 @@
+//
+//  main.c
+//  LogoEquipo
+//
+//  Created by Gail Keegan Lopez Monares on 14/02/20.
+//  Copyright © 2020 Gail Keegan Lopez Monares. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (){
+    
+    printf("              ___              \n");
+    printf("             xXXXx             \n");
+    printf("              |+|              \n");
+    printf("              |+|              \n");
+    printf("          Xxxx***xxxX          \n");
+    printf("             \\|||/             \n");
+    printf("              |||              \n");
+    printf("              |||              \n");
+    printf(" ***************************** \n");
+    printf(" ***************************** \n");
+    printf(" **                         ** \n");
+    printf(" **    Txxxxx-x x-xxxxxT    ** \n");
+    printf(" **   x__X           x__x   ** \n");
+    printf(" **  x                   x  ** \n");
+    printf(" **                         ** \n");
+    printf(" ***************************** \n");
+    printf(" ***************************** \n");
+    printf("              |||              \n");
+    printf("              |||              \n");
+    printf("              |||              \n");
+    printf("              \\!/              \n");
+    
+    getchar();
+    
+    return 0;
+    
+    
+}
