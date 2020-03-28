d322a35357527bd8fb2a11e9f8d8fe686ddb7152
eli <eli_darko@hotmail.com>
2020-03-27 18:48:43 -0600

    version 1 formulario

---
+++ funcion.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void funcion();
+
+int eNumero = 5;
+
+int main() {
+	
+	int eNumero = 10;
+	printf("%d\n", eNumero);
+	
+	funcion();
+	getchar();
+	return 0;
+}
+
+void funcion() {
+	printf("%d", eNumero);
+}
